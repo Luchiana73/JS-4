65b118abf059712371a549e0887c0409df25424b
Svetlana <luchiana.sv@gmail.com>
2023-10-29 14:00:22 +0100

    created a new script monthly expenses - hw 2.4.2

---
+++ monthlyExpenses.js
@@ -0,0 +1,31 @@
+let expencesExamples = [
+  {
+    yearlyExpences: [
+      2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390,
+    ],
+  },
+  {
+    yearlyExpences: [
+      500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200,
+    ],
+  },
+  {
+    yearlyExpences: [
+      20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000,
+    ],
+  },
+];
+
+const options = { month: "long" };
+
+expencesExamples.map((example) => {
+  const monthlyExpenses = [];
+  example.yearlyExpences.map((value, index) => {
+    const date = new Date().getMonth();
+    const month = new Date(date, index).toLocaleString("locale", options);
+    if (value <= 1000) {
+      return monthlyExpenses.push(`${month}: ${value}`);
+    }
+  });
+  console.log(monthlyExpenses);
+});
